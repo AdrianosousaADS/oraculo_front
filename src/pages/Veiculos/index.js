b2c03ccb2867667635aea41edb8ccb938c9f9932
AdrianosousaADS <132022764+AdrianosousaADS@users.noreply.github.com>
2023-06-09 06:51:04 +0000

    ok

--- src/pages/Veiculos/index.js
+++ src/pages/Veiculos/index.js
@@ -1,58 +1,75 @@
 import React, { useState } from 'react';
 import './style.css';
+
+
 function ListaCarros({ onAddCar }) {
-    const [marca, setMarca] = useState('');
-    const [modelo, setModelo] = useState('');
-    const [ano, setAno] = useState('');
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const car = { marca, modelo, ano };
-      onAddCar(car);
+  const [marca, setMarca] = useState('');
+  const [modelo, setModelo] = useState('');
+  const [ano, setAno] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const car = { marca, modelo, ano };
+
+    try {
+      const response = await fetch('/api/cars', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(car),
+      });
+
+      const createdCar = await response.json();
+
+      console.log(createdCar); // Opcional: imprime o carro criado no console
+
+      onAddCar(createdCar);
       setMarca('');
       setModelo('');
       setAno('');
-    };
-  
-    return (
-      <div className="container">
-        <h1> Registro de Veiculo:</h1>
-      <h2>
-      Registre o seu veiculo em nosso sistema e saiba tudo sobre ele !
-      </h2>
-        
-        <form onSubmit={handleSubmit}>
-          <label>
-            Marca:
-            <input
-              type="text"
-              value={marca}
-              onChange={(e) => setMarca(e.target.value)}
-            />
-          </label>
-          <br />
-          <label>
-            Modelo:
-            <input
-              type="text"
-              value={modelo}
-              onChange={(e) => setModelo(e.target.value)}
-            />
-          </label>
-          <br />
-          <label>
-            Ano:
-            <input
-              type="text"
-              value={ano}
-              onChange={(e) => setAno(e.target.value)}
-            />
-          </label>
-          <br />
-          <button type="submit">Adicionar Carro</button>
-        </form>
-      </div>
-    );
-  }
-  
-  export default ListaCarros;
+    } catch (error) {
+      console.error(error); // Opcional: imprime o erro no console
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Registro de Veiculo:</h1>
+      <h2>Registre o seu veiculo em nosso sistema e saiba tudo sobre ele!</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Marca:
+          <input
+            type="text"
+            value={marca}
+            onChange={(e) => setMarca(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Modelo:
+          <input
+            type="text"
+            value={modelo}
+            onChange={(e) => setModelo(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Ano:
+          <input
+            type="text"
+            value={ano}
+            onChange={(e) => setAno(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Adicionar Carro</button>
+      </form>
+    </div>
+  );
+}
+
+export default ListaCarros;
